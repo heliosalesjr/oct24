d336e63ee40b0978f4795cdb2ef16248788e2b64
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-10-09 15:53:02 -0300

    main page basic elements

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <h1>Hola</h1>
+      <>
+        <div className="text-center container my-4 mx-auto">
+          <h1 className="text-3xl mb-4">My Home, Homies!</h1>
+          <div className="flex gap-2 justify-center">
+            <Button>Click me!</Button>
+            <Button>Click me!</Button>
+          </div>
+        </div>
+      </>
     </>
   );
 }
