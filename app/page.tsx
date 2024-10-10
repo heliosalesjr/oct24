882408c623604680098ab2da54ad178df0cc0fca
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-10-10 16:39:34 -0300

    sign in working

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,23 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Button } from "@/components/ui/button"
+import { SignInButton, SignUpButton, UserButton } from "@clerk/nextjs"
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
 
-export default function Home() {
+export default function HomePage() {
+  
+
   return (
-    <>
-      <>
-        <div className="text-center container my-4 mx-auto">
-          <h1 className="text-3xl mb-4">My Home, Homies!</h1>
-          <div className="flex gap-2 justify-center">
-            <Button>Click me!</Button>
-            <Button>Click me!</Button>
-          </div>
-        </div>
-      </>
-    </>
-  );
+    <div className="text-center container my-4 mx-auto">
+      <h1 className="text-3xl mb-4">My home, our home</h1>
+      <div className="flex gap-2 justify-center">
+        <Button asChild>
+          <SignInButton />
+        </Button>
+        <Button asChild>
+          <SignUpButton />
+        </Button>
+        <UserButton />
+      </div>
+    </div>
+  )
 }
