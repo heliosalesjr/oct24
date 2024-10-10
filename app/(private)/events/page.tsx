7e88d51a9a2e10ca8219add0f67af6027bc773ea
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-10-10 19:20:12 -0300

    protecting private pages

---
+++ app/(private)/events/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function EventsPage() {
+  return (
+    <>
+        <h1>EventsPage</h1>
+        <UserButton />
+    </>
+    
+  )
+}
+
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ import { auth } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
 
 export default function HomePage() {
-  
+  const { userId } = auth()
+  if (userId != null) redirect("/events")
 
   return (
     <div className="text-center container my-4 mx-auto">
