1cea9718654eae50c21bd7a878c11cd5786bb150
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-10-11 17:44:05 -0300

    neon & drizzle setup

---
+++ app/drizzle/db.ts
@@ -0,0 +1,7 @@
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+async function main() {
+    const sql = neon(process.env.DATABASE_URL!);
+    const db = drizzle(sql);
+}
+main();
---
+++ app/drizzle/migrations/0000_dashing_newton_destine.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS "events" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" text NOT NULL,
+	"description" text,
+	"durationInMinutes" integer NOT NULL,
+	"clerkUserId" text NOT NULL,
+	"isActive" boolean DEFAULT true NOT NULL,
+	"createdAt" timestamp DEFAULT now() NOT NULL,
+	"updatedAt" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "clerkUserIdIndex" ON "events" USING btree ("clerkUserId");
